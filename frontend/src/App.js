402ccbae9d836dcd336eb590b20a39dd8d3e1737
JoshuaSpicer <joshua.j.spicer2@gmail.com>
2023-06-10 03:51:21 +1000

    Adding in the product lines and home page components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,16 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <div>
       <Header/>
 <main className="py-3">
-<Container> 
-    <h1>Welcome to Wacky Struts</h1>
-</Container>
+  <Container> 
+    <HomeScreen />
+  </Container>
 
 </main>
   
